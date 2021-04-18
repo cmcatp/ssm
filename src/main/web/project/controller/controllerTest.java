c31b32f67dd1be795d534b2495bb711cf48df949
cmca <cmca@tp.com>
2021-04-18 13:06:34 +0800

    2

--- src/main/web/project/controller/controllerTest.java
+++ src/main/web/project/controller/controllerTest.java
@@ -1,14 +1,33 @@
 package project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import project.pojo.person;
+
+import javax.annotation.Resource;
 
 @Controller
 public class controllerTest {
+/*    @Value("1")
+    private int i;*/
+    private person p;
+    @Autowired
+    @Qualifier(value="person")
+    public void setP(person p) {
+        this.p = p;
+    }
+
+    @Resource(type = person.class)
+    private person p2;
+
+
     @RequestMapping("/hello")
     public String test1(){
-        System.out.println("testing....");
+        System.out.println(p);
         return "success";
+
     }
 
 }
---
+++ src/main/web/project/pojo/person.java
@@ -0,0 +1,35 @@
+package project.pojo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class person {
+    public String name;
+    public int age;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    @Override
+    public String toString() {
+        return "person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
